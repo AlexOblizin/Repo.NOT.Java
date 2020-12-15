a7162e746449950ea3c622bb707d59900a097adc
AlexOblizin <shura.oblizin@mail.ru>
2020-12-15 14:04:16 +0300

    some tricks)

--- LearningEPAM/src/LearningEPAM/JavaSE/Main.java
+++ LearningEPAM/src/LearningEPAM/JavaSE/Main.java
@@ -15,4 +15,29 @@ public class Main {
         }
         System.out.println(quizzesCompleted);
     }
+
+    public static double sqr(double arg) {
+        while (true); // Удивительно, но КОМПИЛИРУЕТСЯ!
+    }
+
+    public static double aaaaa(int a) {
+
+    return a;
+    }
+
+
+
+    public static double sqr2(double arg) {
+        throw new RuntimeException(); // тоже КОМПИЛИРУЕТСЯ!
+    }
+    public static int area(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Negative sizes: w = " + width + ", h = " + height);
+        }
+        return width * height;
+    }
+
+
+
 }
+
